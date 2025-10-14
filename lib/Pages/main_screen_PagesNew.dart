90af1c078552c6fe35556e8168f5e494ca35692a
minoru-yamanaka <minoruyamanaka@icloud.com>
2025-10-14 19:16:00 -0300

    ajustes main_screen

--- lib/Pages/main_screen_PagesNew.dart
+++ lib/Pages/main_screen_PagesNew.dart
@@ -1,10 +1,10 @@
-import 'package:atalaia_ar_condicionados_flutter_application/Pages/agenda_page.dart';
 import 'package:flutter/material.dart';
 import 'package:atalaia_ar_condicionados_flutter_application/Pages/calculadora_page.dart';
 import 'package:atalaia_ar_condicionados_flutter_application/Pages/home_page.dart';
 import 'package:atalaia_ar_condicionados_flutter_application/Pages/localizacao_page.dart';
 import 'package:atalaia_ar_condicionados_flutter_application/Pages/info_page.dart';
 import 'package:atalaia_ar_condicionados_flutter_application/Pages/exit_page.dart';
+import 'package:atalaia_ar_condicionados_flutter_application/Pages/agenda_page.dart';
 
 class MainScreen2 extends StatefulWidget {
   const MainScreen2({super.key});
@@ -36,41 +36,36 @@ class _MainScreenState2 extends State<MainScreen2> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
-
       bottomNavigationBar: BottomNavigationBar(
+        // CORREÇÃO APLICADA AQUI
+        type: BottomNavigationBarType.fixed,
+
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.store), label: 'Início'),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.build_circle),
             label: 'Informações',
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.calendar_month),
             label: 'Agenda',
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.calculate_sharp),
             label: 'Calculadora',
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.location_on),
             label: 'Contato',
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.exit_to_app),
-            label:
-                'Sair', // Alterado de 'Exit' para 'Sair' para manter o padrão PT-BR
+            label: 'Sair',
           ),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: const Color(0xFF343B6C),
         unselectedItemColor: Colors.grey,
-        // Garante que os labels sempre apareçam
         showUnselectedLabels: true,
         onTap: _onItemTapped,
       ),
