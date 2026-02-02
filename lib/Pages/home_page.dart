c37565a526bf3e3d5374552b2aa949ee6af2863d
Renato Navarro <renatonm9@gmail.com>
2026-02-02 19:41:43 -0300

    teste notificação

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -132,8 +132,7 @@ class _HomePageState extends State<HomePage> {
                 SizedBox(
                   width: 250, // Largura de cada card no carrossel
                   child: ProductMiniCard(
-                    imagePath:
-                        'assets/img/Ar-condicionado_ como escolher o melhor modelo _ VEJA.png',
+                    imagePath: 'assets/img/Arcondicionado.png',
                     title: 'Split Hi-Wall 9.000 BTUs',
                     description:
                         'Ideal para quartos e escritórios de até 15m².',
@@ -142,8 +141,7 @@ class _HomePageState extends State<HomePage> {
                 SizedBox(
                   width: 250,
                   child: ProductMiniCard(
-                    imagePath:
-                        'assets/img/Ar-condicionado_ como escolher o melhor modelo _ VEJA.png',
+                    imagePath: 'assets/img/Arcondicionado.png',
                     title: 'Multi-Split 18.000 BTUs',
                     description:
                         'Climatize dois ambientes com uma só unidade externa.',
@@ -152,8 +150,7 @@ class _HomePageState extends State<HomePage> {
                 SizedBox(
                   width: 250,
                   child: ProductMiniCard(
-                    imagePath:
-                        'assets/img/Ar-condicionado_ como escolher o melhor modelo _ VEJA.png',
+                    imagePath: 'assets/img/Arcondicionado.png',
                     title: 'Piso-Teto 36.000 BTUs',
                     description: 'Alta potência para lojas e grandes salões.',
                   ),
